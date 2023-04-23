e8a0ea60013520c87d3c5f7441e9688f6152b235
DevD1k <113674752+DevD1k@users.noreply.github.com>
2023-04-23 20:14:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function threeSum(arr, target) {
+function threeSum(dec) {
 // write your code here
+	let bin = '';
+    while(dec>0){
+        let rem = dec%2;
+        bin = rem + bin;
+        dec = Math.floor(dec/2);
+    }
+    return bin;
   
 }
-
 module.exports = threeSum;
